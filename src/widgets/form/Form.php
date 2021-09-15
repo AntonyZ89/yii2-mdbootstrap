17f7a8262b8c2d037db0fe40fa4fe27fb3cd45ec
Antony <antonyz.dev@gmail.com>
2021-09-15 17:01:01 -0300

    $footer and $footerOptions added to `widgets\form\Form`

--- src/widgets/form/Form.php
+++ src/widgets/form/Form.php
@@ -10,26 +10,45 @@ use yii\db\ActiveRecord;
 /**
  * Class Form
  * @package antonyz89\mdb\widgets
- * 
+ *
  * Yii2 MDBootstrap
- * 
+ *
  * @author Antony Gabriel <antonyz.dev@gmail.com>
- * 
+ *
  * @since 1.0.0
  */
 class Form extends Widget
 {
-    /** @var ActiveRecord */
+    /**
+     * @var ActiveRecord 
+     * 
+     * @required if [[$footer]] is null
+     */
     public $model;
+
+    /** @var string|null */
+    public $footer;
+
+    /** @var array */
+    public $footerOptions = ['class' => 'card-footer text-end'];
 
     public function init()
     {
         Card::begin([
-            'footer' => Html::submitButton($this->model->isNewRecord ? 'Cadastrar' : 'Atualizar', ['class' => 'btn-' . ($this->model->isNewRecord ? 'success' : 'primary')]),
-            'footerOptions' => ['class' => 'card-footer text-end']
+            'footer' => $this->getFooter(),
+            'footerOptions' => $this->footerOptions
         ]);
 
         parent::init();
+    }
+
+    public function getFooter(): string
+    {
+        if ($this->footer) {
+            return $this->footer;
+        } else {
+            return Html::submitButton($this->model->isNewRecord ? 'Cadastrar' : 'Atualizar', ['class' => 'btn-' . ($this->model->isNewRecord ? 'success' : 'primary')]);
+        }
     }
 
     public function run()
