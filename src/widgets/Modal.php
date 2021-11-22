6dc9b3d3b90dc6460bdef392d7273413ff85b2c8
AntonyDev <antonyz.dev@gmail.com>
2021-11-22 14:59:23 -0300

    fix - modal incompatibility with select2 input

--- src/widgets/Modal.php
+++ src/widgets/Modal.php
@@ -9,6 +9,10 @@ use yii\helpers\ArrayHelper;
 class Modal extends ModalBase
 {
 
+    public $options = [
+        'tabindex' => false
+    ];
+
     public $closeButton = [
         'type' => 'button',
         'class' => 'btn-close',
